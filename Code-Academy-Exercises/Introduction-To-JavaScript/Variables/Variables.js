1c8880eef4ecdb9304d77e7781efc3a6212fb22b
hitu1690 <hitu5122@gmail.com>
2019-01-06 10:12:54 -0500

    Create- diff Variable   in Javasript

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Variables.js
@@ -0,0 +1,17 @@
+
+
+let changeMe = true;
+
+changeMe = false;
+
+console.log(changeMe);
+
+// Create-Variable let in Javasript
+
+
+
+const entree = 'Enchiladas';
+console.log(entree);
+
+
+// Create-Variable const  in Javasript
