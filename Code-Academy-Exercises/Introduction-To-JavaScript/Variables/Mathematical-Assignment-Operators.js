89c04c01d3203ba44d60e6157f43a36b8a41d96c
hitu1690 <hitu5122@gmail.com>
2019-01-06 10:23:57 -0500

    Mathemetical-Assignment in Javasript

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Mathematical-Assignment-Operators.js
@@ -0,0 +1,9 @@
+let levelUp = 10;
+levelUp += 5;
+let powerLevel = 9001;
+powerLevel-= 100;
+let multiplyMe = 32;
+multiplyMe *= 11;
+let quarterMe = 1152;
+quarterMe /= 4;
+// Mathemetical-Assignment in Javasript    
