09fdb13dbdf4fc92bcedf288bd2aa1b6381f1975
hitu1690 <hitu5122@gmail.com>
2019-01-06 18:57:23 -0500

    Truthy-and Falsy-Assignment in Javasript

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,9 @@
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+// Truthy-and Falsy-Assignment in Javasript   
