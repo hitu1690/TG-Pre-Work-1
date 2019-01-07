60d1ff7d6663011f4ce6ff275b7a36becf101155
hitu1690 <hitu5122@gmail.com>
2019-01-06 20:43:06 -0500

    what-are-funtions in Javasript

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/What-are-functions.js
@@ -0,0 +1,16 @@
+https://s3.amazonaws.com/codecademy-content/courses/learn-javascript-functions/functions_2.gif
+// Area of the first rectangle
+const width1 = 10;
+const height1 = 6;
+const area1 =  width1 * height1;
+
+// Area of the second rectangle
+const width2 = 4;
+const height2 = 9;
+const area2 =  width2 * height2;
+
+// Area of the third rectangle
+const width3 = 10;
+const height3 = 10;
+const area3 =  width3 * height3;
+// what-are-funtions in Javasript
