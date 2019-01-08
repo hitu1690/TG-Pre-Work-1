097d940fdead7657c4162d58caab518dce880053
hitu1690 <hitu5122@gmail.com>
2019-01-08 15:18:04 -0500

    Loops in Javasript

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Loops.js
@@ -0,0 +1,10 @@
+https://s3.amazonaws.com/codecademy-content/courses/learn-javascript-loops/loops_abatraction_v5.svg
+
+
+A loop is a programming tool that repeats a set of instructions until a specified condition, called a stopping condition is reached. As a programmer, you'll find that you rely on loops all the time! You’ll hear the generic term iterate when referring to loops; iterate simply means "to repeat".
+
+When we need to reuse a task in our code, we often bundle that action in a function. Similarly, when we see that a process has to repeat multiple times in a row, we write a loop. Loops allow us to create efficient code that automates processes to make scalable, manageable programs.
+
+As illustrated in the diagram, loops iterate or repeat an action until a specific condition is met. When the condition is met, the loop stops and the computer moves on to the next part of the program.
+
+// Loops in Javasript    
