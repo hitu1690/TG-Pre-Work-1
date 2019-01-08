c23a3f1a9722534565332711707b924684c29d76
hitu1690 <hitu5122@gmail.com>
2019-01-08 15:31:34 -0500

    repeating-Task-Manually in Javasript

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,6 @@
+const vacationSpots = ['Maxico', 'London', 'Australia'];
+''
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+// repeating-Task-Manually in Javasript
