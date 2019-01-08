967e0cbfd95510b264cb365728cc1eff197ddec6
hitu1690 <hitu5122@gmail.com>
2019-01-08 09:06:40 -0500

    Global-Scope in Javasript

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Global-Scope.js
@@ -0,0 +1,10 @@
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+const callMyNightSky = () => {
+	return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+};
+
+console.log(callMyNightSky());
+// Global-Scope in Javasript
